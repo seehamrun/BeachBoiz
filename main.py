624826f95b06714f671e36a6c80dc2eb75ad3a69
Carolyn Mei <carolynymei@yahoo.com>
2018-07-20 16:52:32 -0500

    setup data entry page and main.py
    also renamed things better in database

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import webapp2
+import logging
+import jinja2
+import os
+
+from google.appengine.ext import ndb
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class ShowData(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/data.html')
+        # values = {
+        # }
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
+    ('/data', ShowData),
 ], debug=True)
