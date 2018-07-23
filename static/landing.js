95885b129627b05b65de1fe7c82fa10b34caa853
Asha <ashavivey@gmail.com>
2018-07-23 15:46:54 -0500

    nav buttons

---
+++ static/landing.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+
+
+
+
+  var calcButton = document.querySelector('#calc')
+  calcButton.addEventListener("click", ()=>{
+    alert ("HI!")
+    //jQuery.get("/calc")
+  })
+
+  var calcButton = document.querySelector('#data')
+  calcButton.addEventListener("click", ()=>{
+    alert ("HI!")
+
+    //jQuery.get("/data")
+  })
+
+  // var calcButton = document.querySelector('#login')
+  // calcButton.addEventListener("click", ()=>{
+  //   jQuery.get("/calc")
+  // })
+  //
+  // var calcButton = document.querySelector('#create')
+  // calcButton.addEventListener("click", ()=>{
+  //   jQuery.get("/calc")
+  // })
+
+
+})
