8df7ff857d4dd681dbada3eac61876066aab5c89
Diego Hernandez Nater <dhernandeznater@gmail.com>
2018-07-23 11:50:11 -0500

    Added function to Cost text box and slider

--- static/calc.js
+++ static/calc.js
@@ -20,25 +20,74 @@ function setup()
 
 function test()
 {
+  console.log("in function")
   userInput = Number(inputNumber.value.replace(/\D/g,''));
-  if(electricSlide.value==lastValue)
+  userRate = Number(inputRate.value.replace(/\D/g, ''));
+  userCost = Number(inputCost.value.replace(/\D/g, ''));
+  calcCost = userRate*userInput
+
+
+  if(userCost==0)
+    calcCost=userRate*electricSlide.value
+
+  if(userRate == 0)
   {
-    console.log(userInput>electricSlide.max)
-    if(userInput>electricSlide.max)
-    {
+    alert("Please put in a number for rate")
+    return;
+  }
 
-      electricSlide.max=userInput*1.25
-    }
+  if(userInput>electricSlide.max)
+  {
+    console.log("1")
+    electricSlide.max=userInput*1.25;
+    electricSlide.max = userInput*userRate*1.25
+  }
+
+  if(calcCost>costSlide.max)
+  {
+    console.log("2")
+
+    costSlide.max=calcCost*1.25;
+    electricSlide.max=calcCost/userRate*1.25
+  }
+
+  if(userCost>costSlide.max)
+  {
+    console.log("3")
+
+    costSlide.max=userCost*1.25
+    electricSlide.max=userCost/userRate*1.25
+  }
+  if(userInput!=lastValue && userInput>0)
+  {
     electricSlide.value=userInput;
-
-    console.log(userInput)
-    console.log(electricSlide.value)
+    costSlide.value = userInput*userRate;
+    inputCost.value = userInput*userRate;
     lastValue=userInput
   }
-  else if(userInput==lastValue)
+  else if(electricSlide.value!=lastValue && electricSlide.value!=1)
   {
     inputNumber.value = electricSlide.value
+    costSlide.value = electricSlide.value*userRate
+    inputCost.value = electricSlide.value*userRate
     lastValue=electricSlide.value
+  }
+  else if(userCost/userRate!=lastValue && userCost/userRate!=0)
+  {
+    var amt = userCost/userRate
+    electricSlide.value=amt
+    inputNumber.value = amt
+    costSlide.value = userCost
+    lastValue = amt
+  }
+  else if (costSlide.value/userRate!=lastValue && costSlide.value!=1)
+  {
+    console.log("madeit")
+    var amt = costSlide.value/userRate
+    electricSlide.value = amt
+    inputNumber.value = amt
+    inputCost.value = costSlide.value
+    lastValue = amt
   }
 
 }
